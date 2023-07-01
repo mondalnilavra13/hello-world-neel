697de7f8c98bcac214fa43833e4137f969ac7f41
mondalnilavra13 <126654958+mondalnilavra13@users.noreply.github.com>
2023-07-01 20:33:22 +0530

    1 file modified, 3 added, 2 deleted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import HelloWorld from './HelloWorld';
-import Greeting from './Greeting';
+
+import HelloWorld from './components/HelloWorld';
+import Greeting from './components//Greeting';
+import DataFetcher from  './components/DataFetcher'
 
 function App() {
   return (
     <div>
       <HelloWorld />
       <Greeting />
+      <DataFetcher />
     </div>
   );
 }
---
+++ src/components/DataFetcher.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+
+function DataFetcher() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://restcountries.com/v2/all');
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Data from API:</h2>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default DataFetcher;
